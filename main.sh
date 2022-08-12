837de7ecbf7111a6465d136803aeecedf1bbf9c3
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2022-08-12 15:56:28 +0000

    Add check in bash script

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 clear
 cd Chess
+
+if [ -d "bin/me/abhaydaksh/chess" ]; then
+	java -cp bin me.abhaydaksh.chess.Main
+	exit
+fi
+
 printf "Compiling..."
 javac -d bin src/me/abhaydaksh/chess/*.java src/me/abhaydaksh/chess/pieces/*.java
 if [ $? == 0 ] 
